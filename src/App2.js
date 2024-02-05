fabcf0be1fe5463487d0271044d4d68cce89ee63
Ernest <ernestisibor9@gmail.com>
2024-02-05 05:34:32 -0800

    Registration form in react

--- src/App2.js
+++ src/App2.js
@@ -1,15 +1,17 @@
 import React from 'react'
-import LessonNine from './components/LessonNine'
+// import LessonNine from './components/LessonNine'
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import "../node_modules/bootstrap/dist/js/bootstrap.min.js"
 import { Provider } from 'react-redux'
 import store from './redux/store.js'
+import LessonSeven from './components/LessonSeven.js'
 
 function App2() {
   return (
     <div>
       <Provider store={store}>
-        <LessonNine/>
+        {/* <LessonNine/> */}
+        <LessonSeven/>
       </Provider>
     </div>
   )
